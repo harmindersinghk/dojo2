9054d29bdc77a4b2703a2d8e09d163bc4029a4c1
Harminder Singh <P94279@MacBook-Pro-6.local>
2018-12-20 16:16:20 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const server = require('./lib/server')
-const pkg = require('./package.json')
-const PORT = process.env.PORT || 8001
-const log = require('./lib/log')
-
 var appzip = require('appmetrics-zipkin')({
     host: 'http://51.141.29.230',
     port: 9401,
     serviceName:pkg.name,
     sampleRate: 1.0
   });
+
+const server = require('./lib/server')
+const pkg = require('./package.json')
+const PORT = process.env.PORT || 8001
+const log = require('./lib/log')
 
 server.listen(PORT, () => {
     log().info(`${pkg.name} is running on ${PORT}` )
